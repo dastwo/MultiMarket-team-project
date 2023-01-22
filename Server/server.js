fb9cf3a3c35ed0039752ced020d218b77581a385
dastow <dastwo68@gmail.com>
2023-01-23 00:52:43 +0200

    update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {readdirSync} = require('fs');
 const cors = require('cors');
 const app = express();
 const passport = require('passport');
@@ -27,32 +28,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// ROUTERS
-const storeRouter = require('./routers/storeRouter');
-const userRouter = require('./routers/userRouter');
-const departmentRouter = require('./routers/departmentRouter');
-const couponRouter = require('./routers/couponRouter');
-const orderRouter = require('./routers/orderRouter');
-const productRouter = require('./routers/productsRouter');
-const sectionRouter = require('./routers/sectionRouter');
-const authRouter = require('./routers/authRouter');
-const authPassportRouter = require('./routers/authPassportRouter');
-const adminStoreRouter = require('./routers/adminStoreRouter');
-const messageRouter = require('./routers/messageRouter');
-const stripeRouter = require('./routers/stripeRouter');
-
-app.use('/api/store', storeRouter);
-app.use('/api/user', userRouter);
-app.use('/api/department', departmentRouter);
-app.use('/api/coupon', couponRouter);
-app.use('/api/order', orderRouter);
-app.use('/api/product', productRouter);
-app.use('/api/section', sectionRouter);
-app.use('/api/auth', authRouter);
-app.use('/auth', authPassportRouter);
-app.use('/api/stripe', stripeRouter);
-app.use('/api/ownerStore', adminStoreRouter);
-app.use('/api/messages', messageRouter);
+readdirSync('./routes').map((r)=> app.use('/api', require(`./routes/${r}`)))
 
 const port = 8000;
 
